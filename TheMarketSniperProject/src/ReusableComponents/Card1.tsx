360f1e6871edce2d7c77604ee64c14a2c76018aa
Joao Alegria <joao.alegria@openbrix.co.uk>
2023-10-19 21:20:15 +0100

    HVF Method Card Customization

--- TheMarketSniperProject/src/ReusableComponents/Card1.tsx
+++ TheMarketSniperProject/src/ReusableComponents/Card1.tsx
@@ -1,7 +1,7 @@
 
 export default function Card1({ title, image, description} ) {
   return (
-    <div className="w-3/3 hover:shadow-[0_0_30px_green] rounded-lg duration-300 bg-gray-900">    
+    <div className="w-3/3 hover:shadow-[0_0_50px_green] rounded-lg duration-300 bg-gray-900">    
       <h1 className='text-3xl text-green-400 text-center py-6 capitalize font-bold'>{title}</h1>   
       <img src={image} />
       <h4 className='capitalize text-white text-clip overflow-hidden text-1xl text-center px-2 py-4'>{description}</h4>
--- TheMarketSniperProject/src/ReusableComponents/Card2.tsx
+++ TheMarketSniperProject/src/ReusableComponents/Card2.tsx
@@ -1,13 +1,14 @@
-import HVFLogo from '.././assets/TMS_logo.webp'
+import HVFLogo from '.././assets/BullVsBearWallpaper.jpg'
 
 export default function Card2( ) {
   return (
     <div className="w-3/3 hover:shadow-[0_0_30px_green] rounded-lg duration-300 bg-gray-900">    
-      <h1 className='text-3xl text-green-400 text-center py-6 capitalize font-bold'>{'HVF method course'}</h1>   
-      <div className='bg-white'>
-        <img src={HVFLogo} className='w-3/5 m-auto bg-white' />
+      <h1 className='text-3xl text-green-400 text-center py-6 capitalize font-bold'>{'HVF method'}</h1>   
+      <div className='bg-gray-900'>
+        {/* <img src={HVFLogo} className='w-3/5 m-auto bg-white' /> */}
+        <img src={HVFLogo} className='w-4/5 m-auto' />
       </div>
-      <h4 className='capitalize text-white text-clip overflow-hidden text-1xl text-center px-2 py-4'>{'description here'}</h4>
+      <h4 className='capitalize text-white text-clip overflow-hidden text-1xl text-center px-2 py-4'>{'HVF method course'}</h4>
     </div>
   )
 }
