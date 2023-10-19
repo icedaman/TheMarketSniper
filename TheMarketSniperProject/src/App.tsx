70d45fcf85a8ff7f4654b1c593d83ad3f138fe23
Joao Alegria <joao.alegria@openbrix.co.uk>
2023-10-19 12:24:38 +0100

    TMS logo and images added

--- TheMarketSniperProject/src/App.tsx
+++ TheMarketSniperProject/src/App.tsx
@@ -1,21 +1,42 @@
+import TMSLogo from './assets/Web2-Logo-120TMS.png'
+import TradingDaysLogo from './assets/trading_days_banner.jpg'
+import MarketsLogo from './assets/markets_thumbnail.jpg'
+import JobsLogo from './assets/jobs_thumbnail.jpg'
+import MeetingTimeLogo from './assets/meeting_time_thumbnail.jpg'
+import ReviewTimeLogo from './assets/time_for_review_thumbnail.jpg'
+import BTCLogo from './assets/btc_thumbnail.jpg'
 
 export default function App() {
   return (
-    <div className="w-full min-h-screen container mx-auto py-4 bg-yellow-300">
-      <button className="px-5 py-5 bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 ...">
-        Save changes
-      </button>
-      <div className="grid grid-cols-3 gap-4 bg-green-600 mx-70 h-screen mt-4 mb-4"> 
-        <div className="w-3/3 bg-indigo-700 hover:bg-sky-700">1</div>
-        <div className="w-3/3 bg-indigo-700">2</div>
-        <div className="w-3/3 bg-indigo-700">3</div>
-        <div className="w-3/3 bg-indigo-700">4</div>
-        <div className="w-3/3 bg-indigo-700 dark:md:hover:bg-fuchsia-600 ">5</div>
-        <div className="w-3/3 bg-indigo-700">6</div>
-        <div className="w-3/3 bg-indigo-700">7</div>
-        <div className="w-3/3 bg-indigo-700">8</div>
-        <div className="w-3/3 bg-indigo-700">9</div>
+    <div className="bg-black">
+      <div className="w-full min-h-screen container mx-auto py-9 bg-black">
+        <img src={TMSLogo} />
+        <div className="grid grid-cols-3 gap-10 bg-green- h-screen mt-4 mb-4 max-h-0 mx-auto py-9"> 
+          <div className="w-3/3 bg-indigo-700 hover:bg-sky-700 ">    
+            <img src={TradingDaysLogo} />
+          </div>
+          <div className="w-3/3 bg-indigo-700 hover:bg-sky-700 ">    
+            <img src={MarketsLogo} />
+          </div>
+          <div className="w-3/3 bg-indigo-700 hover:bg-sky-700 ">    
+            <img src={JobsLogo} />
+          </div>
+          <div className="w-3/3 bg-indigo-700 hover:bg-sky-700 ">    
+            <img src={MeetingTimeLogo} />
+          </div>
+          <div className="w-3/3 bg-indigo-700 hover:bg-sky-700 ">    
+            <img src={BTCLogo} />
+          </div>
+          <div className="w-3/3 bg-indigo-700 hover:bg-sky-700 ">    
+            <img src={ReviewTimeLogo} />
+          </div>
+        </div>
       </div>
+        <div>
+          <button className="px-5 py-5 bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 ...">
+            Save changes
+          </button>
+        </div>
     </div>
   )
 }
