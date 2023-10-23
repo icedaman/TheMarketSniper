63b4d99df5a13d637d68d51d56f2f52128ab6e0b
Joao Alegria <joao.alegria@openbrix.co.uk>
2023-10-23 10:38:56 +0100

    back button added

--- TheMarketSniperProject/src/pages/Library.tsx
+++ TheMarketSniperProject/src/pages/Library.tsx
@@ -2,13 +2,16 @@ import CardGeneric from '../ReusableComponents/CardGeneric'
 import TradingDaysLogo from '../assets/trading_days_banner.jpg'
 import MarketsLogo from '../assets/markets_thumbnail.jpg'
 import HVFLogo from '../assets/BullVsBearWallpaper.jpg'
+import { Link } from 'react-router-dom'
 
 export default function Library() {
   return (
     <div className="w-full min-h-screen container mx-auto py-9 bg-transparent">
       <div className="grid grid-cols-3 gap-10  mt-4 mb-4 mx-auto py-36">
         <CardGeneric title={"HVF method"} image={HVFLogo} description={"HVF method course"} />
-        <CardGeneric title={"live trading days"} image={TradingDaysLogo} description={"Join Francis & the Sniper Community in a Live Session"} />
+        <Link to={"/live-sessions"}>
+          <CardGeneric title={"live trading days"} image={TradingDaysLogo} description={"Join Francis & the Sniper Community in a Live Session"} />
+        </Link>
         <CardGeneric title={"stream archives"} image={MarketsLogo} description={"Watch Previous Streams"} />
       </div>
   </div>
--- TheMarketSniperProject/src/pages/LiveTradingSessions.tsx
+++ TheMarketSniperProject/src/pages/LiveTradingSessions.tsx
@@ -2,10 +2,16 @@ import CardGeneric from "../ReusableComponents/CardGeneric"
 import MeetingTimeLogo from '../assets/meeting_time_thumbnail.jpg'
 import JobsLogo from '../assets/jobs_thumbnail.jpg'
 import BTCLogo from '../assets/btc_thumbnail.jpg'
+import { Link } from 'react-router-dom'
 
 export default function LiveTradingSessions() {
   return (
     <div className="w-full min-h-screen container mx-auto py-9 bg-transparent">
+      <>
+        <Link to={"/"}>
+          <button className="text-green-500 p-8 bg-zinc-900 rounded-2xl absolute mt-12">Back</button>
+        </Link>
+      </>  
       <div className="grid grid-cols-3 gap-10  mt-4 mb-4 mx-auto py-36">
       <CardGeneric title={"sunday sessions"} image={MeetingTimeLogo} description={"join in our sunday market analysis session"} />
       <CardGeneric title={"forex and equities"} image={JobsLogo} description={"Forex and Equities focused Live Session"} />
@@ -15,5 +21,3 @@ export default function LiveTradingSessions() {
   </div>
   )
 }
-
-
